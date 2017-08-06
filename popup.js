13ff82a5cc9369dc19cd2d3b1bab72c20b13a290
Marco Acierno <marcoacierno96@gmail.com>
2017-08-06 19:37:38 +0200

    Clean up unused code

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const dictionarySelect = document.querySelector('#dictionary');
+
 dictionarySelect.addEventListener('change', e => {
     chrome.storage.local.set({ dictionary:  dictionarySelect.value });
 });
@@ -6,15 +7,6 @@ dictionarySelect.addEventListener('change', e => {
 chrome.storage.local.get('dictionary', items => {
     const dictionary = items.dictionary;
     dictionarySelect.value = dictionary;
-
-    // chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    //     chrome.tabs.sendMessage(tabs[0].id, {
-    //         action: 'inject_text',
-    //         data: {
-    //             dictionary,
-    //         },
-    //     });
-    // });
 
     chrome.runtime.sendMessage({
         action: 'popup_open',
